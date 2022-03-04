db6a347e131e87d10f7d3086dd016ed1094b7acb
Moise-Nturubika <nturumoussa@gmail.com>
2022-03-04 14:39:56 +0200

    Custom client model

--- client/models.py
+++ client/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Client(models.Model):
     fullname = models.CharField(db_column="fullname", max_length=100)
     phone = models.CharField(db_column="phone", max_length=20)
+    image = models.FileField(db_column="image", upload_to='client/profil/', null=True)
     lastConnection = models.DateTimeField(db_column="lastConnection")
 
     class Meta:
