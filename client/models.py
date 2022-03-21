47a566183108379701d75c72570f3da609fc734b
Moise-Nturubika <nturumoussa@gmail.com>
2022-03-21 15:54:14 +0200

    Add password field to client model

--- client/models.py
+++ client/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Client(models.Model):
     fullname = models.CharField(db_column="fullname", max_length=100)
     phone = models.CharField(db_column="phone", max_length=20)
+    password = models.CharField(db_column="password", max_length=255)
     image = models.FileField(db_column="image", upload_to='client/profil/', null=True)
     lastConnection = models.DateTimeField(db_column="lastConnection")
 
